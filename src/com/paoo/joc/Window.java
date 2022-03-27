d58e4fe712b51103cbc26bae421871d763a116a6
Eduard-Adrian <101168962+Eduard-Adrian@users.noreply.github.com>
2022-03-27 16:19:43 +0300

    Add files via upload
    Window + Game Loop

---
+++ src/com/paoo/joc/Window.java
@@ -0,0 +1,16 @@
+package com.paoo.joc;
+
+import javax.swing.*;
+
+public class Window extends JFrame{
+
+    public Window () {
+        setTitle("Epic Home Invasion");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(new GamePanel(1280, 720));
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
