828c43d88858a44918f1d707c88e66505a107236
Eduard-Adrian <101168962+Eduard-Adrian@users.noreply.github.com>
2022-03-29 23:52:43 +0300

    Add files via upload
    Am adaugat MouseInput, Vector2f, si scheletul pentru alte 3 game states + modificat GamePanel

---
+++ src/com/paoo/joc/states/GameOverState.java
@@ -0,0 +1,27 @@
+package com.paoo.joc.states;
+
+import com.paoo.joc.input.KeyInput;
+import com.paoo.joc.input.MouseInput;
+
+import java.awt.*;
+
+public class GameOverState extends GameState {
+    public GameOverState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void input(MouseInput mouse, KeyInput key) {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+}
---
+++ src/com/paoo/joc/states/PauseState.java
@@ -0,0 +1,27 @@
+package com.paoo.joc.states;
+
+import com.paoo.joc.input.KeyInput;
+import com.paoo.joc.input.MouseInput;
+
+import java.awt.*;
+
+public class PauseState extends GameState {
+    public PauseState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void input(MouseInput mouse, KeyInput key) {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+}
