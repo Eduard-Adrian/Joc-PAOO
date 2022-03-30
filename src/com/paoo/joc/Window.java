1f66379c2328e34be7b05ce25894da8222fa0dd1
Eduard-Adrian <101168962+Eduard-Adrian@users.noreply.github.com>
2022-03-31 01:32:47 +0300

    Add files via upload
    +incarcare spriteuri (player + animatii)
    +schelet coliziuni

--- src/com/paoo/joc/Window.java
+++ src/com/paoo/joc/Window.java
@@ -7,7 +7,7 @@ public class Window extends JFrame{
     public Window () {
         setTitle("Epic Home Invasion");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(new GamePanel(1280, 720));
+        setContentPane(new GamePanel(720, 480));
 
         pack();
         setLocationRelativeTo(null);
--- src/com/paoo/joc/states/PlayState.java
+++ src/com/paoo/joc/states/PlayState.java
@@ -2,30 +2,33 @@ package com.paoo.joc.states;
 
 
 
+import com.paoo.joc.entity.Player;
+import com.paoo.joc.graphics.Sprite;
 import com.paoo.joc.input.KeyInput;
 import com.paoo.joc.input.MouseInput;
+import com.paoo.joc.util.Vector2f;
 
 import java.awt.Graphics2D;
-import java.awt.Color;
+
 
 public class PlayState extends GameState {
 
+    private Player player;
+
     public PlayState (GameStateManager gsm){
         super(gsm);
+        player = new Player(new Sprite("Entity/b.png",64,64), new Vector2f(100,100), 64);
     }
 
     public void update() {
-
+        player.update();
     }
 
     public void render(Graphics2D g) {
-        g.setColor(Color.red);
-        g.fillRect(20,20,100,100);
+        player.render(g);
     }
 
     public void input(MouseInput mouse, KeyInput key) {
-        if(key.up.down){
-            System.out.println("aaaaaaaaaaaaaa");
-        }
+        player.input(mouse, key);
     }
 }
